3af1c18507e4d1ee3499ec4534dc1884e97b75e6
Tecci <tecci.fi@gmail.com>
2026-09-08 21:01:43 +0300

    sp00ph suggestions

--- src/board/movegen.rs
+++ src/board/movegen.rs
@@ -114,7 +114,7 @@ impl Board {
             let dest = Square::new(file, Rank::Sixth.relative_to(self.stm));
             let victim = Square::new(file, Rank::Fifth.relative_to(self.stm));
 
-            // `EnPassant` already calculated all the legal en pheasants
+            // `calc_en_passant` already calculated all the legal en pheasants
             for src in en_passant.attackers(self.stm) {
                 for duck in empty ^ src ^ dest ^ victim {
                     moves.push(Move::new(src, dest, duck, MoveFlag::EnPassant));
@@ -130,9 +130,11 @@ impl Board {
 
         for src in knights {
             for dest in valid_dest & knight_attacks(src) {
-                let flag = self
-                    .piece_on(dest)
-                    .map_or(MoveFlag::Normal, |_| MoveFlag::Capture);
+                let flag = if self.piece_on(dest).is_some() {
+                    MoveFlag::Capture
+                } else {
+                    MoveFlag::Normal
+                };
                 for duck in empty & !dest ^ src {
                     moves.push(Move::new(src, dest, duck, flag));
                 }
@@ -148,9 +150,11 @@ impl Board {
 
         for src in diag {
             for dest in valid_dest & bishop_attacks(blockers, src, self.slider_tag) {
-                let flag = self
-                    .piece_on(dest)
-                    .map_or(MoveFlag::Normal, |_| MoveFlag::Capture);
+                let flag = if self.piece_on(dest).is_some() {
+                    MoveFlag::Capture
+                } else {
+                    MoveFlag::Normal
+                };
                 for duck in empty & !dest ^ src {
                     moves.push(Move::new(src, dest, duck, flag));
                 }
@@ -159,9 +163,11 @@ impl Board {
 
         for src in orth {
             for dest in valid_dest & rook_attacks(blockers, src, self.slider_tag) {
-                let flag = self
-                    .piece_on(dest)
-                    .map_or(MoveFlag::Normal, |_| MoveFlag::Capture);
+                let flag = if self.piece_on(dest).is_some() {
+                    MoveFlag::Capture
+                } else {
+                    MoveFlag::Normal
+                };
                 for duck in empty & !dest ^ src {
                     moves.push(Move::new(src, dest, duck, flag));
                 }
@@ -175,9 +181,11 @@ impl Board {
         let king = self.king(self.stm);
 
         for dest in valid_dest & king_attacks(king) {
-            let flag = self
-                .piece_on(dest)
-                .map_or(MoveFlag::Normal, |_| MoveFlag::Capture);
+            let flag = if self.piece_on(dest).is_some() {
+                MoveFlag::Capture
+            } else {
+                MoveFlag::Normal
+            };
             for duck in empty & !dest ^ king {
                 moves.push(Move::new(king, dest, duck, flag));
             }
@@ -207,56 +215,61 @@ impl Board {
     }
 }
 
-#[test]
-fn pawn_attack() {
-    const EXPECTED_RESULT: usize = 8 * (64 - 6) + 2 * (64 - 5);
-    let board = Board::from_fen("8/3k4/8/2p*p3/3P4/8/3K4/8 w - - 0 1")
-        .expect("board couldnt parse fen string");
-    board.display(true);
-    assert_eq!(board.gen_moves().len(), EXPECTED_RESULT);
-}
+#[cfg(test)]
+mod tests {
+    use crate::board::Board;
 
-#[test]
-fn knight_corners() {
-    const EXPECTED_RESULT: usize = 13 * (64 - 7);
-    let board = Board::from_fen("N3k2N/8/8/4*3/8/8/8/N3K2N w - - 0 1")
-        .expect("board couldnt parse fen string");
-    board.display(true);
-    assert_eq!(board.gen_moves().len(), EXPECTED_RESULT);
-}
+    #[test]
+    fn pawn_attack() {
+        const EXPECTED_RESULT: usize = 8 * (64 - 6) + 2 * (64 - 5);
+        let board = Board::from_fen("8/3k4/8/2p*p3/3P4/8/3K4/8 w - - 0 1")
+            .expect("board couldnt parse fen string");
+        board.display(true);
+        assert_eq!(board.gen_moves().len(), EXPECTED_RESULT);
+    }
 
-#[test]
-fn knight_captures_and_blockers() {
-    const EXPECTED_RESULT: usize = 20 * (64 - 7) + 2 * (64 - 6);
-    let board = Board::from_fen("7k/7n/4*3/1r3R2/3N4/8/8/K7 w - - 0 1")
-        .expect("board couldnt parse fen string");
-    board.display(true);
-    assert_eq!(board.gen_moves().len(), EXPECTED_RESULT);
-}
+    #[test]
+    fn knight_corners() {
+        const EXPECTED_RESULT: usize = 13 * (64 - 7);
+        let board = Board::from_fen("N3k2N/8/8/4*3/8/8/8/N3K2N w - - 0 1")
+            .expect("board couldnt parse fen string");
+        board.display(true);
+        assert_eq!(board.gen_moves().len(), EXPECTED_RESULT);
+    }
 
-#[test]
-fn king_empty() {
-    const EXPECTED_RESULT: usize = 8 * (64 - 3);
-    let board =
-        Board::from_fen("8/4K3/8/4*3/8/8/4k3/8 b - - 0 1").expect("board couldnt parse fen string");
-    board.display(true);
-    assert_eq!(board.gen_moves().len(), EXPECTED_RESULT);
-}
+    #[test]
+    fn knight_captures_and_blockers() {
+        const EXPECTED_RESULT: usize = 20 * (64 - 7) + 2 * (64 - 6);
+        let board = Board::from_fen("7k/7n/4*3/1r3R2/3N4/8/8/K7 w - - 0 1")
+            .expect("board couldnt parse fen string");
+        board.display(true);
+        assert_eq!(board.gen_moves().len(), EXPECTED_RESULT);
+    }
 
-#[test]
-fn king_capture() {
-    const EXPECTED_RESULT: usize = 8 * (64 - 10);
-    let board = Board::from_fen("7*/8/8/3PPP2/3PkP2/3PPP2/8/K7 b - - 0 1")
-        .expect("board couldnt parse fen string");
-    board.display(true);
-    assert_eq!(board.gen_moves().len(), EXPECTED_RESULT);
-}
+    #[test]
+    fn king_empty() {
+        const EXPECTED_RESULT: usize = 8 * (64 - 3);
+        let board = Board::from_fen("8/4K3/8/4*3/8/8/4k3/8 b - - 0 1")
+            .expect("board couldnt parse fen string");
+        board.display(true);
+        assert_eq!(board.gen_moves().len(), EXPECTED_RESULT);
+    }
 
-#[test]
-fn king_castling() {
-    const EXPECTED_RESULT: usize = 23 * (64 - 7) + (64 - 6);
-    let board = Board::from_fen("8/1k6/8/8/3*4/6n1/8/R1n1K2R w KQ - 0 1")
-        .expect("board couldnt parse fen string");
-    board.display(true);
-    assert_eq!(board.gen_moves().len(), EXPECTED_RESULT);
+    #[test]
+    fn king_capture() {
+        const EXPECTED_RESULT: usize = 8 * (64 - 10);
+        let board = Board::from_fen("7*/8/8/3PPP2/3PkP2/3PPP2/8/K7 b - - 0 1")
+            .expect("board couldnt parse fen string");
+        board.display(true);
+        assert_eq!(board.gen_moves().len(), EXPECTED_RESULT);
+    }
+
+    #[test]
+    fn king_castling() {
+        const EXPECTED_RESULT: usize = 23 * (64 - 7) + (64 - 6);
+        let board = Board::from_fen("8/1k6/8/8/3*4/6n1/8/R1n1K2R w KQ - 0 1")
+            .expect("board couldnt parse fen string");
+        board.display(true);
+        assert_eq!(board.gen_moves().len(), EXPECTED_RESULT);
+    }
 }
