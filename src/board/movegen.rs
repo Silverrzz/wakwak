a00c2f263dd0fbb0a69a2348e183f904931d8d2e
Ethan <ethan.dally@live.co.uk>
2026-09-08 15:06:31 +0100

    king and tests

--- src/board/movegen.rs
+++ src/board/movegen.rs
@@ -1,14 +1,11 @@
 use crate::board::Board;
 use crate::common::{
-    Bitboard, Color, East, Move, MoveFlag, MoveList, North, Piece, Rank, South, Square, West,
-    knight_attacks,
+    Bitboard, Color, East, Move, MoveFlag, MoveList, North, Piece, Rank, South, Square, West, king_attacks, knight_attacks, rook_rays
 };
 
 impl Board {
     #[inline]
-    fn duck_bb(&self, src: Square, dest: Square, flag: MoveFlag) -> Bitboard {
-        let mut empty_square_bb = !self.colors(Color::White) & !self.colors(Color::Black);
-
+    fn duck_moves(&self, mut empty_square_bb: Bitboard, src: Square, dest: Square, flag: MoveFlag) -> Bitboard {
         match flag {
             MoveFlag::Capture | MoveFlag::DoublePush | MoveFlag::Normal => {
                 empty_square_bb.0 |= 0 << src as usize;
@@ -24,13 +21,13 @@ impl Board {
         let mut list = MoveList::default();
         let friendly_bb = self.colors(self.stm());
         let enemy_bb = self.colors(!self.stm());
-        let filled_square_bb = enemy_bb | friendly_bb;
+        let duck_bb = self.duck().map_or(Bitboard::EMPTY, Square::bitboard);
+        let filled_square_bb = enemy_bb | friendly_bb | duck_bb;
         let empty_square_bb = !filled_square_bb;
 
         /*
         Pawns
         */
-
         let friendly_pawns = self.pieces(Piece::Pawn) & friendly_bb;
         let pawns_forward_1 = match self.stm() {
             Color::Black => friendly_pawns.shift::<South>(1),
@@ -49,7 +46,7 @@ impl Board {
                 Color::White => dest.offset_dir::<North>(-1),
                 Color::Black => dest.offset_dir::<South>(-1),
             };
-            self.duck_bb(src, dest, flag).iter().for_each(|duck| {
+            self.duck_moves(empty_square_bb, src, dest, flag).iter().for_each(|duck| {
                 list.add(Move::new(src, dest, duck, flag));
             });
         });
@@ -63,7 +60,7 @@ impl Board {
                 Color::White => dest.offset_dir::<North>(-2),
                 Color::Black => dest.offset_dir::<South>(-2),
             };
-            self.duck_bb(src, dest, flag).iter().for_each(|duck| {
+            self.duck_moves(empty_square_bb, src, dest, flag).iter().for_each(|duck| {
                 list.add(Move::new(src, dest, duck, flag));
             });
         });
@@ -77,7 +74,7 @@ impl Board {
                 Color::Black => dest.offset(1, -1),
                 Color::White => dest.offset(1, 1),
             };
-            self.duck_bb(src, dest, flag).iter().for_each(|duck| {
+            self.duck_moves(empty_square_bb, src, dest, flag).iter().for_each(|duck| {
                 list.add(Move::new(src, dest, duck, flag));
             });
         });
@@ -91,16 +88,15 @@ impl Board {
                 Color::Black => dest.offset(-1, -1),
                 Color::White => dest.offset(-1, 1),
             };
-            self.duck_bb(src, dest, flag).iter().for_each(|duck| {
+            self.duck_moves(empty_square_bb, src, dest, flag).iter().for_each(|duck| {
                 list.add(Move::new(src, dest, duck, flag));
             });
         });
 
-        //En Passant (work already done for us)
+        //En Passant
         if let Some(en_passant) = self.en_passant() {
             let flag = MoveFlag::EnPassant;
             let dest = Square::new(en_passant.file(), Rank::Sixth.relative_to(self.stm()));
-
             'left: {
                 if !en_passant.left() {
                     break 'left;
@@ -109,11 +105,10 @@ impl Board {
                     break 'left;
                 };
                 let src_left = Square::new(left, Rank::Fifth.relative_to(self.stm()));
-                self.duck_bb(src_left, dest, flag).iter().for_each(|duck| {
+                self.duck_moves(empty_square_bb, src_left, dest, flag).iter().for_each(|duck| {
                     list.add(Move::new(src_left, dest, duck, flag));
                 });
             }
-
             'right: {
                 if !en_passant.right() {
                     break 'right;
@@ -122,14 +117,17 @@ impl Board {
                     break 'right;
                 };
                 let src_left = Square::new(left, Rank::Fifth.relative_to(self.stm()));
-                self.duck_bb(src_left, dest, flag).iter().for_each(|duck| {
+                self.duck_moves(empty_square_bb, src_left, dest, flag).iter().for_each(|duck| {
                     list.add(Move::new(src_left, dest, duck, flag));
                 });
             }
         }
 
+        /*
+        Knights
+        */
+
         // Knights cant end on a friendly piece or the duck, uses precomputed attacks (ty tecci)
-        let duck_bb = self.duck().map_or(Bitboard::EMPTY, Square::bitboard);
         let knight_targets = !friendly_bb & !duck_bb;
         for src in self.pieces(Piece::Knight) & friendly_bb {
             // The compile-time lookup table already handles board edges.
@@ -139,56 +137,81 @@ impl Board {
                 } else {
                     MoveFlag::Normal
                 };
-                for duck in self.duck_bb(src, dest, flag) {
+                for duck in self.duck_moves(empty_square_bb, src, dest, flag) {
                     list.add(Move::new(src, dest, duck, flag));
                 }
             }
         }
 
+        /*
+        King
+        */
+        let king_sq = (self.pieces(Piece::King) & friendly_bb).next();
+        let king_attacks = king_attacks(king_sq);
+        king_attacks.iter().for_each(|dest|{
+            let flag = match enemy_bb.has(dest) {
+                true => MoveFlag::Capture,
+                false => MoveFlag::Normal
+            };
+            self.duck_moves(empty_square_bb, king_sq, dest, flag).iter().for_each(|duck| {
+                list.add(Move::new(king_sq, dest, duck, flag));
+            });
+        });
+
         list
     }
 }
 
-// FIXME: these numbers need to be updated as movegen is completed more :D
-
-#[test]
-fn perft_depth_1() {
-    let board = Board::from_fen("rnbqkbnr/pppppppp/8/8/4*3/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
-        .expect("board couldnt parse fen string");
-    assert_eq!(board.gen_moves().len(), 640);
-}
-
 #[test]
 fn pawn_attack() {
-    let board = Board::from_fen("4k3/8/8/2p1p3/3P4/8/8/3K4 w - - 0 1")
+    const EXPECTED_RESULT: usize = 8 * (64 - 6) + 2 * (64 - 5);
+    let board = Board::from_fen("8/3k4/8/2p*p3/3P4/8/3K4/8 w - - 0 1")
         .expect("board couldnt parse fen string");
-    assert_eq!(board.gen_moves().len(), 179);
-}
-
-#[test]
-fn en_passant() {
-    let board = Board::from_fen("4k3/8/8/2p1p3/3P4/8/8/3K4 w - - 0 1")
-        .expect("board couldnt parse fen string");
-    assert_eq!(board.gen_moves().len(), 179);
+    board.display(true);
+    assert_eq!(board.gen_moves().len(), EXPECTED_RESULT);
 }
 
 #[test]
 fn knight_corners() {
-    let board = Board::from_fen("N3k2N/8/8/8/8/8/8/N3K2N w - - 0 1")
+    const EXPECTED_RESULT: usize = 13 * (64 - 7);
+    let board = Board::from_fen("N3k2N/8/8/4*3/8/8/8/N3K2N w - - 0 1")
         .expect("board couldnt parse fen string");
-    assert_eq!(board.gen_moves().len(), 464);
+    board.display(true);
+    assert_eq!(board.gen_moves().len(), EXPECTED_RESULT);
 }
 
 #[test]
 fn knight_captures_and_blockers() {
+    const EXPECTED_RESULT: usize = 2 * (64 - 6) + 15 * (64 - 7);
     let board = Board::from_fen("7k/7n/4*3/1r3R2/3N4/8/8/K7 w - - 0 1")
         .expect("board couldnt parse fen string");
-    assert_eq!(board.gen_moves().len(), 349);
+    board.display(true);
+    assert_eq!(board.gen_moves().len(), EXPECTED_RESULT);
 }
 
 #[test]
 fn black_knight_captures_and_blockers() {
-    let board = Board::from_fen("7K/7N/4*3/1R3r2/3n4/8/8/k7 b - - 0 1")
+    const EXPECTED_RESULT: usize = 2 * (64 - 6) + 15 * (64 - 7);
+    let board: Board = Board::from_fen("7K/7N/4*3/1R3r2/3n4/8/8/k7 b - - 0 1")
         .expect("board couldnt parse fen string");
-    assert_eq!(board.gen_moves().len(), 349);
+    board.display(true);
+    assert_eq!(board.gen_moves().len(), EXPECTED_RESULT);
+}
+
+#[test]
+fn king_empty() {
+    const EXPECTED_RESULT: usize = 8 * (64 - 3);
+    let board = Board::from_fen("8/4K3/8/4*3/8/8/4k3/8 b - - 0 1")
+        .expect("board couldnt parse fen string");
+    board.display(true);
+    assert_eq!(board.gen_moves().len(), EXPECTED_RESULT);
+}
+
+#[test]
+fn king_capture() {
+    const EXPECTED_RESULT: usize = 8 * (64 - 10);
+    let board = Board::from_fen("7*/8/8/3PPP2/3PkP2/3PPP2/8/K7 b - - 0 1")
+        .expect("board couldnt parse fen string");
+    board.display(true);
+    assert_eq!(board.gen_moves().len(), EXPECTED_RESULT);
 }
