d15447cc84787805802b8460c253c33eb68b1161
Ethan <ethan.dally@live.co.uk>
2026-09-09 17:31:07 +0100

    initial negamax

--- src/board/search.rs
+++ src/board/search.rs
@@ -1,8 +1,8 @@
-use std::cmp::{max, Ordering};
+use std::cmp::{max_by_key};
 use rand::{RngExt, rngs::ThreadRng};
 use crate::{board::Board, common::Move};
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy)]
 struct SearchOutcome{
     score: i32,
     mv: Option<Move>
@@ -13,31 +13,35 @@ fn eval(board: &Board, rng: &mut ThreadRng) -> i32 {
     rng.random::<i32>()
 }
 
+fn start_negamax(board: Board, depth: u8) -> SearchOutcome {
+    let mut rng = rand::rng();
+    negamax(SearchOutcome { score: i32::MIN, mv: None}, board, depth, &mut rng)
+}
+
 #[inline]
-fn oubbbsmax(outcome: SearchOutcome, board: Board, depth: u8, rng: &mut ThreadRng) -> SearchOutcome {
+fn negamax(outcome: SearchOutcome, board: Board, depth: u8, rng: &mut ThreadRng) -> SearchOutcome {
     if depth == 0 {
         return outcome;
     }
     let moves = board.gen_moves();
     if moves.is_empty() {
-        return Some(SearchOutcome { score: i32::MAX, mv: None });
+        return SearchOutcome { score: i32::MAX, mv: None };
     }
-    let best_move: Option<Move> = None;
+    let mut best_outcome: SearchOutcome = SearchOutcome{mv: None, score: i32::MIN};
     for mv in moves.iter() {
         let mut new_board = board.clone();
         let outcome = SearchOutcome{mv: Some(*mv), score: eval(&board, rng)};
         new_board.make_move(*mv);
-        let new_outcome = oubbbsmax(outcome, new_board, depth, rng);
-        best_move = Some(max(v1, v2))
+        let new_outcome = negamax(outcome, new_board, depth - 1, rng);
+        best_outcome = max_by_key(best_outcome, new_outcome, |o: &SearchOutcome| o.score);
     }
-    todo!()
+    best_outcome
 }
 
 #[test]
 fn depth_1() {
-    let mut rng = rand::rng();
     let board = Board::from_fen("7*/8/8/3PPP2/3PkP2/3PPP2/8/K7 b - - 0 1")
         .expect("board couldnt parse fen string");
-    let eval = wakamax(board, 1, &mut rng);
-    assert_eq!(Eval(0), eval);
+    println!("{:?}", start_negamax(board, 1));
+    assert!(false);
 }
