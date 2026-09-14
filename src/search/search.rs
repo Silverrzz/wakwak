16df4ac86eff3a1583a93be91743da852e6fcaa1
Tecci <tecci.fi@gmail.com>
2026-09-15 01:51:01 +0300

    no rfp "in check"
    Bench: 604326

--- src/search/search.rs
+++ src/search/search.rs
@@ -214,8 +214,7 @@ fn search<Node: NodeType>(
         }
     }
 
-    // TODO: uncomment this when it is used
-    // let in_check = pos.board().in_check();
+    let in_check = pos.board().in_check();
     let raw_eval = eval(pos.board());
     let corr = thread.history.corr(pos.board());
     let static_eval = adjust_eval(raw_eval, corr);
@@ -231,6 +230,7 @@ fn search<Node: NodeType>(
     be reasonably confident that a further search will also fail high.
     */
     if !Node::ROOT
+        && !in_check
         && depth <= Params::rfp_depth()
         && static_eval - Params::rfp_margin(depth) >= beta
     {
