99be27cc08f5232204198beee415cda4e3c4cd80
Tecci <tecci.fi@gmail.com>
2026-09-15 10:51:03 +0300

    maybe it's just too aggressive
    Bench: 1722279

--- src/search/search.rs
+++ src/search/search.rs
@@ -300,8 +300,8 @@ fn search<Node: NodeType>(
             let mut score = -Score::INFINITE;
             if !Node::PV || legal_moves > 1 {
                 let reduction = if depth >= 3 && searched_moves > 6 && is_quiet {
-                    let ldr = if safe == Bitboard::FULL {
-                        duck_counts[src][dest].ilog(3) as i32
+                    let ldr = if safe == Bitboard::FULL && duck_counts[src][dest] > 3 {
+                        1
                     } else {
                         0
                     };
