6331e588d708dca47ac7b573f558179af1cc02d1
Tecci <tecci.fi@gmail.com>
2026-09-15 10:43:19 +0300

    maybe only do LDR when safe is full like LDP y'know
    Bench: 1635529

--- src/search/search.rs
+++ src/search/search.rs
@@ -300,7 +300,13 @@ fn search<Node: NodeType>(
             let mut score = -Score::INFINITE;
             if !Node::PV || legal_moves > 1 {
                 let reduction = if depth >= 3 && searched_moves > 6 && is_quiet {
-                    1 + duck_counts[src][dest].ilog(3) as i32
+                    let ldr = if safe == Bitboard::FULL {
+                        duck_counts[src][dest].ilog(3) as i32
+                    } else {
+                        0
+                    };
+
+                    1 + ldr
                 } else {
                     0
                 };
