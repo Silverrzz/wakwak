cb8d6dc3a6f7cc4361ebfc06f2acffbb61830e27
kelseyde <daniel.kelsey@hotmail.co.uk>
2026-09-18 12:52:20 +0200

    bench 351015

--- src/search/search.rs
+++ src/search/search.rs
@@ -447,7 +447,7 @@ fn search<Node: NodeType>(
             let mut score = -Score::INFINITE;
             if !Node::PV || legal_moves > 1 {
                 let reduction = if depth >= 3 && searched_moves > 6 && is_quiet {
-                    1 + depth / 4 + !improving as i32
+                    1 + depth / 4 + searched_moves / 12 + !improving as i32
                 } else {
                     0
                 };
