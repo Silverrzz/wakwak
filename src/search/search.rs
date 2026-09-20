199aea229b03a3161c43ba1947a86a84e3558b6a
Tecci <tecci.fi@gmail.com>
2026-09-20 17:38:03 +0300

    DCP in PV nodes
    Bench: 604842

--- src/search/search.rs
+++ src/search/search.rs
@@ -426,8 +426,7 @@ fn search<Node: NodeType>(
             given duck move, we can be reasonably confident that any move containing
             that duck won't be much better, so we can skip the rest of them
             */
-            if !Node::PV
-                && is_quiet
+            if is_quiet
                 && depth <= Params::dcp_depth()
                 && duck_counts[duck] >= Params::dcp_threshold(depth, improving) as u8
             {
