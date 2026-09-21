0e76f07ed2f58422fa57c6c0b0ea776c398000d1
Ellie <lewisfulterer@gmail.com>
2026-09-21 01:34:59 +0100

    update download net
    bench 1260085

--- download_net.py
+++ download_net.py
@@ -5,8 +5,8 @@ import hashlib
 import os
 
 def main():
-    name = "sourdough"
-    hash = "53f4e38872e0ac2cac02ff99aea68336621cf5bf39a06d934062c1fa856114f5"
+    name = "brioche"
+    hash = "70dec0ba0ba7ee6df281b65feedf4b84d8543f61bf9eada1a7e21a3dfb8e6131"
     path = "./nets/wakwak.nnue"
 
     os.makedirs(os.path.dirname(path), exist_ok=True)
