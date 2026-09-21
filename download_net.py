1e6367a9622a80b61c5569e8dadd5848469bd2c7
Ellie <lewisfulterer@gmail.com>
2026-09-21 13:52:35 +0100

    update download net
    bench 1037567

--- download_net.py
+++ download_net.py
@@ -5,8 +5,8 @@ import hashlib
 import os
 
 def main():
-    name = "pretzel"
-    hash = "4e1ee45c9e0eee0ed0b6ebf7b67af50f04d195c450b614e17e23792051694ad7"
+    name = "pretzel_2"
+    hash = "9b22d6b686dce13d999b28fe5c8eff97e5484132b897b74d0ad08c808661cad4"
     path = "./nets/wakwak.nnue"
 
     os.makedirs(os.path.dirname(path), exist_ok=True)
